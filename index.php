d52d633a540bfc43a9b6a7c7545b510add5f6800
nmieza <nurmieza14@gmail.com>
2022-06-17 01:15:36 +0800

    commit

--- index.php
+++ index.php
@@ -194,3 +194,28 @@ session_start();
     </div>
 </body>
 
+<body>
+<nav class="menu-container">
+  <input type="checkbox" aria-label="Toggle menu" />
+  <span></span>
+  <span></span>
+  <span></span>
+
+  <a href="studentpage.php" class="menu-logo">
+    <img src="logo.png"  alt="My Awesome Website"/>
+  </a>
+  <div class="menu">
+    <ul>
+      <li>
+        <a>
+          WELCOME TO ASSIGNMENT SUBMISSION SYSTEM
+        </a>
+      </li>
+    </ul>
+    
+  </div>
+</nav>
+ <div class="footer">
+ <p>© COPYRIGHT UNIVERSITI TEKNOLOGI MARA 2022 CAWANGAN TERENGGANU - KAMPUS KUALA TERENGGANU</p>
+</div>    
+</body>
