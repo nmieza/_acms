61975a4d8f6d13cfa7ad3c64fb65319984bf97a7
nmieza <nurmieza14@gmail.com>
2022-06-20 23:16:15 +0800

    commit

--- index.php
+++ index.php
@@ -186,7 +186,7 @@ session_start();
     </style>
     </head>
 <body>
-    <?php include 'nav.php'; ?>
+    <?php include 'nav_.php'; ?>
     <div class="login-box">
         <h2>AIR CARGO MANAGEMENT SYSTEM<br><br>Log in as?</h2>
         <form>
@@ -197,4 +197,67 @@ session_start();
 
 </body>
 </html>
+</html>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AIR CARGO MANAGEMENT SYSTEM</title>
+    <body class="hold-transition login-page">
+    <style>
+
+    html{
+    height: 100%;
+}
+ body {
+    margin: 0;
+    background-image: linear-gradient(rgba(0,0,0,.5), rgba(0,0,0,.5)), url(ap.jpg);
+    background-position: center;
+    background-size: cover;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+}
+
+.topnav {
+  overflow: hidden;
+  background-color: #333;
+}
+
+.topnav a {
+  float: left;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.topnav a.active {
+  background-color: #04AA6D;
+  color: white;
+}
+
+</style>
+</head>
+<body>
+
+<div class="topnav">
+  <a class="active" href="#home">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a>
+</div>
+
+
+</body>
 </html>
