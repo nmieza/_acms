b9c1a1aec14fc09d00c8cd99df7bb1895cf529a3
nmieza <nurmieza14@gmail.com>
2022-06-20 23:45:22 +0800

    commit

--- index.php
+++ index.php
@@ -21,7 +21,6 @@ session_start();
     min-height: 100vh;
     background-image: linear-gradient(rgba(0,0,0,.5), rgba(0,0,0,.5)), url(ap.jpg);
     background-position: center;
-    background-size: cover;
     display: flex;
 
 }
