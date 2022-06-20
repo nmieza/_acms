651c2256bc2b5595207701c2c0a303d5f999219a
nmieza <nurmieza14@gmail.com>
2022-06-20 22:57:33 +0800

    commit

--- index.php
+++ index.php
@@ -182,6 +182,30 @@ session_start();
     }
 }
 
+    </style>
+    </head>
+<body>
+    <?php include 'home_topnav.php'; ?>
+    <div class="login-box">
+        <h2>AIR CARGO MANAGEMENT SYSTEM<br><br>Log in as?</h2>
+        <form>
+        <a href="wh_login.php" target="_self"><span></span><span></span><span></span><span></span>WAREHOUSE</a>
+        <a href="ap_login.php" target="_self"><span></span><span></span><span></span><span></span>AIRPORT</a>
+    </form>
+    </div>
+
+</body>
+</html>
+
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
 .topnav {
   overflow: hidden;
   background-color: #333;
@@ -205,27 +229,21 @@ session_start();
   background-color: #04AA6D;
   color: white;
 }
-
-    </style>
-    </head>
+</style>
+</head>
 <body>
-    
-    <div class="login-box">
-        <h2>AIR CARGO MANAGEMENT SYSTEM<br><br>Log in as?</h2>
-        <form>
-        <a href="wh_login.php" target="_self"><span></span><span></span><span></span><span></span>WAREHOUSE</a>
-        <a href="ap_login.php" target="_self"><span></span><span></span><span></span><span></span>AIRPORT</a>
-    </form>
-    </div>
 
-    <div class="topnav">
-        <a class="active" href="#home">Home</a>
-        <a href="#news">News</a>
-        <a href="#contact">Contact</a>
-        <a href="#about">About</a>
-    </div>
+<div class="topnav">
+  <a class="active" href="#home">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a>
+</div>
+
+<div style="padding-left:16px">
+  <h2>Top Navigation Example</h2>
+  <p>Some content..</p>
+</div>
 
 </body>
-
-
 </html>
