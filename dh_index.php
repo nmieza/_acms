1a5716869979ff9dfb2b47e72264bfbb7bd40c11
nmieza <nurmieza14@gmail.com>
2022-06-20 22:09:13 +0800

    commit

--- dh_index.php
+++ dh_index.php
@@ -1,118 +1,136 @@
-</style>
-<!-- Main Sidebar Container -->
-      <aside class="main-sidebar sidebar-dark-primary elevation-4 sidebar-no-expand">
-        <!-- Brand Logo -->
-        <a href="<?php echo base_url ?>index.php" class="brand-link bg-gradient-primary text-sm">
-        <img src="<?php echo validate_image($_settings->info('logo'))?>" alt="Store Logo" class="brand-image img-circle elevation-3" style="opacity: .8;width: 1.5rem;height: 1.5rem;max-height: unset">
-        <span class="brand-text font-weight-light"><?php echo $_settings->info('short_name') ?></span>
-        </a>
-        <!-- Sidebar -->
-        <div class="sidebar os-host os-theme-light os-host-overflow os-host-overflow-y os-host-resize-disabled os-host-transition os-host-scrollbar-horizontal-hidden">
-          <div class="os-resize-observer-host observed">
-            <div class="os-resize-observer" style="left: 0px; right: auto;"></div>
-          </div>
-          <div class="os-size-auto-observer observed" style="height: calc(100% + 1px); float: left;">
-            <div class="os-resize-observer"></div>
-          </div>
-          <div class="os-content-glue" style="margin: 0px -8px; width: 249px; height: 646px;"></div>
-          <div class="os-padding">
-            <div class="os-viewport os-viewport-native-scrollbars-invisible" style="overflow-y: scroll;">
-              <div class="os-content" style="padding: 0px 8px; height: 100%; width: 100%;">
-                <!-- Sidebar user panel (optional) -->
-                <div class="clearfix"></div>
-                <!-- Sidebar Menu -->
-                <nav class="mt-4">
-                   <ul class="nav nav-pills nav-sidebar flex-column text-sm nav-compact nav-flat nav-child-indent nav-collapse-hide-child" data-widget="treeview" role="menu" data-accordion="false">
-                    <li class="nav-item dropdown">
-                      <a href="./" class="nav-link nav-home">
-                        <i class="nav-icon fas fa-tachometer-alt"></i>
-                        <p>
-                          Dashboard
-                        </p>
-                      </a>
-                    </li> 
-                    <li class="nav-item dropdown">
-                      <a href="<?php echo base_url ?>admin/?page=transactions/manage_transaction" class="nav-link nav-transactions_manage_transaction">
-                        <i class="nav-icon fas fa-plus"></i>
-                        <p>
-                          Add New Shipment
-                        </p>
-                      </a>
-                    </li>
-                    <li class="nav-item dropdown">
-                      <a href="<?php echo base_url ?>admin/?page=transactions" class="nav-link nav-transactions">
-                        <i class="nav-icon fas fa-truck-loading"></i>
-                        <p>
-                          Shipment List
-                        </p>
-                      </a>
-                    </li>
-                    <?php if($_settings->userdata('type') == 1): ?>
-                    <li class="nav-header">Maintenance</li>
-                    <li class="nav-item dropdown">
-                      <a href="<?php echo base_url ?>admin/?page=cargo_types" class="nav-link nav-cargo_types">
-                        <i class="nav-icon fas fa-th-list"></i>
-                        <p>
-                          Cargo Types
-                        </p>
-                      </a>
-                    </li>
-                    <li class="nav-item dropdown">
-                      <a href="<?php echo base_url ?>admin/?page=user/list" class="nav-link nav-user_list">
-                        <i class="nav-icon fas fa-users-cog"></i>
-                        <p>
-                          Users
-                        </p>
-                      </a>
-                    </li>
-                    <li class="nav-item dropdown">
-                      <a href="<?php echo base_url ?>admin/?page=system_info" class="nav-link nav-system_info">
-                        <i class="nav-icon fas fa-cogs"></i>
-                        <p>
-                          Settings
-                        </p>
-                      </a>
-                    </li>
-                    <?php endif; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Air Cargo Shipment Tracking</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <style>
+    /* Set height of the grid so .sidenav can be 100% (adjust as needed) */
+    .row.content {height: 550px}
+    
+    /* Set gray background color and 100% height */
+    .sidenav {
+      background-color: #f1f1f1;
+      height: 100%;
+    }
+        
+    /* On small screens, set height to 'auto' for the grid */
+    @media screen and (max-width: 767px) {
+      .row.content {height: auto;} 
+    }
+  </style>
+</head>
+<body>
 
-                  </ul>
-                </nav>
-                <!-- /.sidebar-menu -->
-              </div>
-            </div>
+<nav class="navbar navbar-inverse visible-xs">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#">Logo</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Dashboard</a></li>
+        <li><a href="#">Add New Shipment</a></li>
+        <li><a href="#">Shipment List</a></li>
+        <li><a href="#">Cargo Type</a></li>
+        <li><a href="#">Users</a></li>
+        <li><a href="#">Settings</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<div class="container-fluid">
+  <div class="row content">
+    <div class="col-sm-3 sidenav hidden-xs">
+      <br>
+      <img src="logoAirCargo.jpg" alt="Flowers in Chania" width="100" height="100">
+      <br><br>
+      <ul class="nav nav-pills nav-stacked">
+        <li class="active"><a href="http://aircargo.test/">Dashboard</a></li>
+        <li><a href="http://aircargo.test/addNewShipment.php">Add New Shipment</a></li>
+        <li><a href="http://aircargo.test/shipmentList.php">Shipment List</a></li>
+        <li><a href="http://aircargo.test/cargoTypes.php">Cargo Type</a></li>
+        <li><a href="http://aircargo.test/users.php">Users</a></li>
+        <li><a href="http://aircargo.test/settings.php">Settings</a></li>
+      </ul><br>
+    </div>
+    <br>
+    
+    <div class="col-sm-9">
+      <div class="well">
+        <h3>Welcome to Air Cargo Shipment Tracking</h3>
+      </div>
+      <div class="row">
+        <div class="col-sm-3">
+          <div class="well">
+            <h4>Users</h4>
+            <p>1 Million</p> 
           </div>
-          <div class="os-scrollbar os-scrollbar-horizontal os-scrollbar-unusable os-scrollbar-auto-hidden">
-            <div class="os-scrollbar-track">
-              <div class="os-scrollbar-handle" style="width: 100%; transform: translate(0px, 0px);"></div>
-            </div>
-          </div>
-          <div class="os-scrollbar os-scrollbar-vertical os-scrollbar-auto-hidden">
-            <div class="os-scrollbar-track">
-              <div class="os-scrollbar-handle" style="height: 55.017%; transform: translate(0px, 0px);"></div>
-            </div>
-          </div>
-          <div class="os-scrollbar-corner"></div>
         </div>
-        <!-- /.sidebar -->
-      </aside>
-      <script>
-    $(document).ready(function(){
-      var page = '<?php echo isset($_GET['page']) ? $_GET['page'] : 'home' ?>';
-      var s = '<?php echo isset($_GET['s']) ? $_GET['s'] : '' ?>';
-      page = page.replace(/\//g,'_');
-      console.log(page)
+        <div class="col-sm-3">
+          <div class="well">
+            <h4>Pages</h4>
+            <p>100 Million</p> 
+          </div>
+        </div>
+        <div class="col-sm-3">
+          <div class="well">
+            <h4>Sessions</h4>
+            <p>10 Million</p> 
+          </div>
+        </div>
+        <div class="col-sm-3">
+          <div class="well">
+            <h4>Bounce</h4>
+            <p>30%</p> 
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm-4">
+          <div class="well">
+            <p>Text</p> 
+            <p>Text</p> 
+            <p>Text</p> 
+          </div>
+        </div>
+        <div class="col-sm-4">
+          <div class="well">
+            <p>Text</p> 
+            <p>Text</p> 
+            <p>Text</p> 
+          </div>
+        </div>
+        <div class="col-sm-4">
+          <div class="well">
+            <p>Text</p> 
+            <p>Text</p> 
+            <p>Text</p> 
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm-8">
+          <div class="well">
+            <p>Text</p> 
+          </div>
+        </div>
+        <div class="col-sm-4">
+          <div class="well">
+            <p>Text</p> 
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 
-      if($('.nav-link.nav-'+page).length > 0){
-             $('.nav-link.nav-'+page).addClass('active')
-        if($('.nav-link.nav-'+page).hasClass('tree-item') == true){
-            $('.nav-link.nav-'+page).closest('.nav-treeview').siblings('a').addClass('active')
-          $('.nav-link.nav-'+page).closest('.nav-treeview').parent().addClass('menu-open')
-        }
-        if($('.nav-link.nav-'+page).hasClass('nav-is-tree') == true){
-          $('.nav-link.nav-'+page).parent().addClass('menu-open')
-        }
-
-      }
-      $('.nav-link.active').addClass('bg-gradient-primary')
-    })
-  </script>
+</body>
+</html>
