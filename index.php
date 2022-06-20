6be52868a9f1687c7cdce904205001cd76399132
nmieza <nurmieza14@gmail.com>
2022-06-20 23:10:39 +0800

    commit

--- index.php
+++ index.php
@@ -22,8 +22,36 @@ session_start();
     background-image: linear-gradient(rgba(0,0,0,.5), rgba(0,0,0,.5)), url(ap.jpg);
     background-position: center;
     background-size: cover;
-    
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
 }
+
+.topnav {
+  overflow: hidden;
+  background-color: #333;
+}
+
+.topnav a {
+  float: left;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.topnav a.active {
+  background-color: #04AA6D;
+  color: white;
+}
+
 
 .login-box {
     position: absolute;
