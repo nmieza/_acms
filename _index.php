726fdf45fc443bc5e6b404d56693b8a6717a69e1
nmieza <nurmieza14@gmail.com>
2022-06-21 00:30:23 +0800

    commit

--- _index.php
+++ _index.php
@@ -5,9 +5,13 @@ session_start();
 
 <!DOCTYPE html>
 <html lang="en">
-	<head><title>AIR CARGO MANAGEMENT SYSTEM</title>
-	<meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1">
-	<style>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AIR CARGO MANAGEMENT SYSTEM</title>
+    <body class="hold-transition login-page">
+    <style>
+        
 	html {
   height: 100%;
 }
