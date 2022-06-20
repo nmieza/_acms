e730495e54c8a9d44ab9180585e550c69465eb17
nmieza <nurmieza14@gmail.com>
2022-06-21 00:36:44 +0800

    commit

--- _index.php
+++ _index.php
@@ -18,7 +18,6 @@ session_start();
 body {
   margin:0;
   padding:0;
-  font-family: sans-serif;
   background:linear-gradient(rgba(0,0,0,.5), rgba(0,0,0,.5)), url(ap.jpg); 
   align-items: center;
 }
