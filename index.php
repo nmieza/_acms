500200301e55bd74811c8a2f580faebc0a4bd76b
nmieza <nurmieza14@gmail.com>
2022-06-21 00:27:31 +0800

    commit

--- index.php
+++ index.php
@@ -186,9 +186,6 @@ session_start();
     </head>
 <body>
     <?php include 'log_.php'; ?>
-    <div class="col-8">
-            <a href="http://localhost/acms/">Go to Website</a>
-    </div>
     <div class="login-box">
         <h2>AIR CARGO MANAGEMENT SYSTEM<br><br>Log in as?</h2>
         <form>
