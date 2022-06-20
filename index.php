c43f3474b20c84de2822587eb1a4f338f5957c5c
nmieza <nurmieza14@gmail.com>
2022-06-20 22:54:59 +0800

    commit

--- index.php
+++ index.php
@@ -182,16 +182,47 @@ session_start();
     }
 }
 
+.topnav {
+  overflow: hidden;
+  background-color: #333;
+}
+
+.topnav a {
+  float: left;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.topnav a.active {
+  background-color: #04AA6D;
+  color: white;
+}
+
     </style>
     </head>
 <body>
-    <?php include 'home_topnav.php'; ?>
+    
     <div class="login-box">
         <h2>AIR CARGO MANAGEMENT SYSTEM<br><br>Log in as?</h2>
         <form>
         <a href="wh_login.php" target="_self"><span></span><span></span><span></span><span></span>WAREHOUSE</a>
         <a href="ap_login.php" target="_self"><span></span><span></span><span></span><span></span>AIRPORT</a>
     </form>
+    </div>
+
+    <div class="topnav">
+        <a class="active" href="#home">Home</a>
+        <a href="#news">News</a>
+        <a href="#contact">Contact</a>
+        <a href="#about">About</a>
     </div>
 
 </body>
