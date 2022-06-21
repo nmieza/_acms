90a25c24254a5aec719557db2fd108911d937f1e
nmieza <nurmieza14@gmail.com>
2022-06-21 23:26:30 +0800

    commit

--- log.php
+++ log.php
@@ -54,7 +54,7 @@ session_start();
 <body>
 
 <div class="topnav">
-  <a class="active" href="#home">Home</a>
+  <a class="active" href="index.php">Home</a>
   <a href="#news">Trace</a>
   <a href="#contact">Contact</a>
   <a href="#about">About</a>
