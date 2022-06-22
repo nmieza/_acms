6fddc7c2a726774f08654a3b4a59cf96d0bfa2cd
nmieza <nurmieza14@gmail.com>
2022-06-22 22:51:32 +0800

    commit

--- log.php
+++ log.php
@@ -1,4 +1,25 @@
-<body style="height: auto;">
+<style>
+    #main-header{
+        position:relative;
+        background: rgb(0,0,0)!important;
+        background: radial-gradient(circle, rgba(0,0,0,0.48503151260504207) 22%, rgba(0,0,0,0.39539565826330536) 49%, rgba(0,212,255,0) 100%)!important;
+    }
+    #main-header:before{
+        content:"";
+        position:absolute;
+        top:0;
+        left:0;
+        width:100%;
+        height:100%;
+        background-image:url(http://localhost/_acms/uploads/cover-1645494240.jpg?v=1645494240);
+        background-repeat: no-repeat;
+        background-size: cover;
+        filter: drop-shadow(0px 7px 6px black);
+        z-index:-1;
+    }
+
+ </style>
+ <body style="height: auto;">
 <nav class="navbar navbar-expand-lg navbar-dark bg-gradient-primary">
             <div class="container px-4 px-lg-5 ">
                 <button class="navbar-toggler btn btn-sm" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
