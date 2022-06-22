9a414f7efb563fd3f176694fc9dd31823e52b437
nmieza <nurmieza14@gmail.com>
2022-06-22 23:29:00 +0800

    commit

--- log.php
+++ log.php
@@ -1,94 +1,62 @@
-<html lang="en" style="height: auto;"><head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-  	<title>Air Cargo Management System</title>
-    <link rel="icon" href="http://localhost/_acms/uploads/logo-1645494239.jpg?v=1645494239">
-    <!-- Google Font: Source Sans Pro -->
-    <!-- <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&amp;display=fallback"> -->
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="http://localhost/_acms/plugins/fontawesome-free/css/all.min.css">
-    <!-- Ionicons -->
-    <!-- <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css"> -->
-    <!-- Tempusdominus Bootstrap 4 -->
-    <link rel="stylesheet" href="http://localhost/_acms/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
-      <!-- DataTables -->
-  <link rel="stylesheet" href="http://localhost/_acms/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
-  <link rel="stylesheet" href="http://localhost/_acms/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
-  <link rel="stylesheet" href="http://localhost/_acms/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
-   <!-- Select2 -->
-  <link rel="stylesheet" href="http://localhost/_acms/plugins/select2/css/select2.min.css">
-  <link rel="stylesheet" href="http://localhost/_acms/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
-    <!-- iCheck -->
-    <link rel="stylesheet" href="http://localhost/_acms/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
-    <!-- JQVMap -->
-    <link rel="stylesheet" href="http://localhost/_acms/plugins/jqvmap/jqvmap.min.css">
-    <!-- Theme style -->
-    <link rel="stylesheet" href="http://localhost/_acms/dist/css/adminlte.css">
-    <link rel="stylesheet" href="http://localhost/_acms/dist/css/custom.css">
-    <link rel="stylesheet" href="http://localhost/_acms/assets/css/styles.css">
-    <!-- overlayScrollbars -->
-    <link rel="stylesheet" href="http://localhost/_acms/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
-    <!-- Daterange picker -->
-    <link rel="stylesheet" href="http://localhost/_acms/plugins/daterangepicker/daterangepicker.css">
-    <!-- summernote -->
-    <link rel="stylesheet" href="http://localhost/_acms/plugins/summernote/summernote-bs4.min.css">
-     <!-- SweetAlert2 -->
-  <link rel="stylesheet" href="http://localhost/_acms/plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css">
-    <style type="text/css">/* Chart.js */
-      @keyframes chartjs-render-animation{from{opacity:.99}to{opacity:1}}.chartjs-render-monitor{animation:chartjs-render-animation 1ms}.chartjs-size-monitor,.chartjs-size-monitor-expand,.chartjs-size-monitor-shrink{position:absolute;direction:ltr;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1}.chartjs-size-monitor-expand>div{position:absolute;width:1000000px;height:1000000px;left:0;top:0}.chartjs-size-monitor-shrink>div{position:absolute;width:200%;height:200%;left:0;top:0}
-    </style>
+<?php
+error_reporting(0);
+session_start();
+?>
 
-     <!-- jQuery -->
-    <script src="http://localhost/_acms/plugins/jquery/jquery.min.js"></script>
-    <!-- jQuery UI 1.11.4 -->
-    <script src="http://localhost/_acms/plugins/jquery-ui/jquery-ui.min.js"></script>
-    <!-- SweetAlert2 -->
-    <script src="http://localhost/_acms/plugins/sweetalert2/sweetalert2.min.js"></script>
-    <!-- Toastr -->
-    <script src="http://localhost/_acms/plugins/toastr/toastr.min.js"></script>
-    <script>
-        var _base_url_ = 'http://localhost/_acms/';
-    </script>
-    <script src="http://localhost/_acms/dist/js/script.js"></script>
-    <script src="http://localhost/_acms/assets/js/scripts.js"></script>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AIR CARGO MANAGEMENT SYSTEM</title>
+    <body class="hold-transition login-page">
     <style>
-    #main-header{
-        position:relative;
-        background: rgb(0,0,0)!important;
-        background: radial-gradient(circle, rgba(0,0,0,0.48503151260504207) 22%, rgba(0,0,0,0.39539565826330536) 49%, rgba(0,212,255,0) 100%)!important;
-    }
-    #main-header:before{
-        content:"";
-        position:relative;
-        top:0;
-        left:0;
-        width:100%;
-        height:100%;
-        background-image:url(http://localhost/_acms/uploads/cover-1645494240.jpg?v=1645494240);
-        background-repeat: no-repeat;
-        background-size: cover;
-        filter: drop-shadow(0px 7px 6px black);
-        z-index:-1;
-    }
 
- </style>
-  </head><body style="height: auto;">
-<nav class="navbar navbar-expand-lg navbar-dark bg-gradient-primary">
-            <div class="container px-4 px-lg-5 ">
-                <button class="navbar-toggler btn btn-sm" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-                <a class="navbar-brand" href="./">
-                <img src="http://localhost/_acms/uploads/logo-1645494239.jpg?v=1645494239" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy">
-                AIR CARGO MANAGEMENT SYSTEM                </a>
-                
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
-                        <li class="nav-item"><a class="nav-link text-white" aria-current="page" href="index.php">Home</a></li>
-                        <li class="nav-item"><a class="nav-link text-white" href="_trace.php">Trace Shipment</a></li>
-                        <li class="nav-item"><a class="nav-link text-white" href="./?p=about">About</a></li>
-                    </ul>
-                </div>
-                <div>
-                  <a href="index.php" class="text-decoration-none text-reset"><b>Login</b></a>
-                </div>
-            </div>
-        </nav>
+    html{
+    height: 100%;
+}
+ body {
+    margin: 0;
+    padding:0;
+
+}
+
+.topnav {
+    position: relative;
+  display: flex;
+  align-items: center;
+  overflow: hidden;
+  background-color: #333;
+}
+
+.topnav a {
+  float: left;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.topnav a.active {
+  background-color: #04AA6D;
+  color: white;
+}
+
+</style>
+</head>
+<body>
+<div class="topnav">
+  <a class="active" href="index.php">Home</a>
+  <a href="_trace.php">Trace Shipment</a>
+  <a href="#about">About</a>
+</div>
+
+</body>
+</html>
