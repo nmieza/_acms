50cf00868f7aee177e0c9afd5f9a23cb6d41a1b5
nmieza <nurmieza14@gmail.com>
2022-06-22 23:38:56 +0800

    commit

---
+++ _log.php
@@ -0,0 +1,51 @@
+<html lang="en" style="height: auto;"><head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  	<title>Air Cargo Management System</title>
+    <link rel="icon" href="http://localhost/_acms/uploads/logo-1645494239.jpg?v=1645494239">
+    <!-- Google Font: Source Sans Pro -->
+    <style type="text/css">/* Chart.js */
+      @keyframes chartjs-render-animation{from{opacity:.99}to{opacity:1}}.chartjs-render-monitor{animation:chartjs-render-animation 1ms}.chartjs-size-monitor,.chartjs-size-monitor-expand,.chartjs-size-monitor-shrink{position:absolute;direction:ltr;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1}.chartjs-size-monitor-expand>div{position:absolute;width:1000000px;height:1000000px;left:0;top:0}.chartjs-size-monitor-shrink>div{position:absolute;width:200%;height:200%;left:0;top:0}
+    </style>
+
+    <style>
+    #main-header{
+        position:relative;
+        background: rgb(0,0,0)!important;
+        background: radial-gradient(circle, rgba(0,0,0,0.48503151260504207) 22%, rgba(0,0,0,0.39539565826330536) 49%, rgba(0,212,255,0) 100%)!important;
+    }
+    #main-header:before{
+        content:"";
+        position:relative;
+        top:0;
+        left:0;
+        width:100%;
+        height:100%;
+        background-image:url(http://localhost/_acms/uploads/cover-1645494240.jpg?v=1645494240);
+        background-repeat: no-repeat;
+        background-size: cover;
+        filter: drop-shadow(0px 7px 6px black);
+        z-index:-1;
+    }
+
+ </style>
+  </head><body style="height: auto;">
+<nav class="navbar navbar-expand-lg navbar-dark bg-gradient-primary">
+            <div class="container px-4 px-lg-5 ">
+                <button class="navbar-toggler btn btn-sm" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <a class="navbar-brand" href="./">
+                <img src="http://localhost/_acms/uploads/logo-1645494239.jpg?v=1645494239" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy">
+                AIR CARGO MANAGEMENT SYSTEM                </a>
+                
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
+                        <li class="nav-item"><a class="nav-link text-white" aria-current="page" href="index.php">Home</a></li>
+                        <li class="nav-item"><a class="nav-link text-white" href="_trace.php">Trace Shipment</a></li>
+                        <li class="nav-item"><a class="nav-link text-white" href="./?p=about">About</a></li>
+                    </ul>
+                </div>
+                <div>
+                  <a href="index.php" class="text-decoration-none text-reset"><b>Login</b></a>
+                </div>
+            </div>
+        </nav>
