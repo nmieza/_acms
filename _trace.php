8cc993a8a0c9ad49ed4ccb7f657a347b13edbff3
nmieza <nurmieza14@gmail.com>
2022-06-22 22:45:58 +0800

    commit

--- _trace.php
+++ _trace.php
@@ -74,6 +74,23 @@
  </style>
   </head><body style="height: auto;">
 <nav class="navbar navbar-expand-lg navbar-dark bg-gradient-primary">
+            <div class="container px-4 px-lg-5 ">
+                <button class="navbar-toggler btn btn-sm" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <a class="navbar-brand" href="./">
+                <img src="http://localhost/_acms/uploads/logo-1645494239.jpg?v=1645494239" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy">
+                AIR CARGO MANAGEMENT SYSTEM                </a>
+                
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
+                        <li class="nav-item"><a class="nav-link text-white" aria-current="page" href="./">Home</a></li>
+                        <li class="nav-item"><a class="nav-link text-white" href="./?p=trace_form">Trace Shipment</a></li>
+                        <li class="nav-item"><a class="nav-link text-white" href="./?p=about">About</a></li>
+                    </ul>
+                </div>
+                <div>
+                  <a href="./admin" class="text-decoration-none text-reset"><b>Admin Panel</b></a>
+                </div>
+            </div>
         </nav>
 <script>
   $(function(){
@@ -154,7 +171,6 @@
                 </div>
             </div>
             <div class="container px-4 px-lg-5 mt-5">
-            <?php include 'log.php'; ?>
                 <form action="" id="trace-frm">
                     <div class="card card-outline card-primary rounded-0 shadow">
                         <div class="card-body">
