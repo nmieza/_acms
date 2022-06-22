7008bd001709a009aeef623310337167ba44ef27
nmieza <nurmieza14@gmail.com>
2022-06-22 23:03:45 +0800

    commit

--- _trace.php
+++ _trace.php
@@ -1,7 +1,7 @@
 <html lang="en" style="height: auto;"><head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-  	<title>Mobile Comparison Website</title>
+  	<title>Air Cargo Management System</title>
     <link rel="icon" href="http://localhost/_acms/uploads/logo-1645494239.jpg?v=1645494239">
     <!-- Google Font: Source Sans Pro -->
     <!-- <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&amp;display=fallback"> -->
