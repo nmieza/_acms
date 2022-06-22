430130604ff4664b6b40da530482fc428120236d
nmieza <nurmieza14@gmail.com>
2022-06-22 23:09:58 +0800

    commit

---
+++ _log.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-gradient-primary">
+            <div class="container px-4 px-lg-5 ">
+                <button class="navbar-toggler btn btn-sm" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <a class="navbar-brand" href="./">
+                <img src="http://localhost/acms/uploads/logo-1645494239.jpg?v=1645494239" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy">
+                MCW - PHP                </a>
+                
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
+                        <li class="nav-item"><a class="nav-link text-white" aria-current="page" href="./">Home</a></li>
+                        <li class="nav-item"><a class="nav-link text-white" href="./?p=trace_form">Trace Shipment</a></li>
+                        <li class="nav-item"><a class="nav-link text-white" href="./?p=about">About</a></li>
+                    </ul>
+                </div>
+                <div>
+                  <a href="./admin" class="text-decoration-none text-reset"><b>Admin Panel</b></a>
+                </div>
+            </div>
+        </nav>
--- log.php
+++ log.php
@@ -60,46 +60,4 @@ session_start();
 </div>
 
 </body>
-
-<style>
-    #main-header{
-        position:relative;
-        background: rgb(0,0,0)!important;
-        background: radial-gradient(circle, rgba(0,0,0,0.48503151260504207) 22%, rgba(0,0,0,0.39539565826330536) 49%, rgba(0,212,255,0) 100%)!important;
-    }
-    #main-header:before{
-        content:"";
-        position:absolute;
-        top:0;
-        left:0;
-        width:100%;
-        height:100%;
-        background-image:url(http://localhost/_acms/uploads/cover-1645494240.jpg?v=1645494240);
-        background-repeat: no-repeat;
-        background-size: cover;
-        filter: drop-shadow(0px 7px 6px black);
-        z-index:-1;
-    }
-
- </style>
-  </head><body style="height: auto;">
-<nav class="navbar navbar-expand-lg navbar-dark bg-gradient-primary">
-            <div class="container px-4 px-lg-5 ">
-                <button class="navbar-toggler btn btn-sm" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-                <a class="navbar-brand" href="./">
-                <img src="http://localhost/_acms/uploads/logo-1645494239.jpg?v=1645494239" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy">
-                AIR CARGO MANAGEMENT SYSTEM                </a>
-                
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
-                        <li class="nav-item"><a class="nav-link text-white" aria-current="page" href="index.php">Home</a></li>
-                        <li class="nav-item"><a class="nav-link text-white" href="_trace.php">Trace Shipment</a></li>
-                        <li class="nav-item"><a class="nav-link text-white" href="./?p=about">About</a></li>
-                    </ul>
-                </div>
-                <div>
-                  <a href="index.php" class="text-decoration-none text-reset"><b>Login</b></a>
-                </div>
-            </div>
-        </nav>
 </html>
