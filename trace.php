6408874262dec57c20ebb10a9934fe2105fddf63
nmieza <nurmieza14@gmail.com>
2022-06-23 18:54:36 +0800

    commit

--- trace.php
+++ trace.php
@@ -40,43 +40,7 @@
   
 </head><body class="sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed sidebar-closed sidebar-collapse" style="height: auto;">
 <div class="wrapper">
-  <!-- Navbar -->
-  <nav class="main-header navbar navbar-expand navbar-primary navbar-dark ">
-   
-            <li class="nav-item">
-        <a class="nav-link" data-widget="pushmenu" href="" role="button"><i class="fas fa-bars"></i></a>
-      </li>
-          <li>
-        <a class="nav-link text-white" href="./" role="button"> <large><b>Courier Management System</b></large></a>
-      </li>
-    </ul>
-
-    <ul class="navbar-nav ml-auto">
-     
-      <li class="nav-item">
-        <a class="nav-link" data-widget="fullscreen" href="#" role="button">
-          <i class="fas fa-expand-arrows-alt"></i>
-        </a>
-      </li>
-     <li class="nav-item dropdown">
-            <a class="nav-link" data-toggle="dropdown" aria-expanded="true" href="javascript:void(0)">
-              <span>
-                <div class="d-felx badge-pill">
-                  <span class="fa fa-user mr-2"></span>
-                  <span><b>Administrator</b></span>
-                  <span class="fa fa-angle-down ml-2"></span>
-                </div>
-              </span>
-            </a>
-            <div class="dropdown-menu" aria-labelledby="account_settings" style="left: -2.5em;">
-              <a class="dropdown-item" href="javascript:void(0)" id="manage_account"><i class="fa fa-cog"></i> Manage Account</a>
-              <a class="dropdown-item" href="ajax.php?action=logout"><i class="fa fa-power-off"></i> Logout</a>
-            </div>
-      </li>
-    </ul>
-  </nav>
  
-   
   </aside>
   <script>
   	$(document).ready(function(){
